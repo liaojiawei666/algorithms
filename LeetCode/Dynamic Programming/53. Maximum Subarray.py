8a5c63563522b4e9bfca71c03430bd9efba9d09c
liaojiawei666 <37116545+liaojiawei666@users.noreply.github.com>
2018-12-13 18:49:47 +0800

    divide

---
+++ LeetCode/Dynamic Programming/53. Maximum Subarray.py
@@ -0,0 +1,23 @@
+# Given an integer array nums, find the contiguous subarray (containing at least one number) which has the largest sum and return its sum.
+#
+# Example:
+#
+# Input: [-2,1,-3,4,-1,2,1,-5,4],
+# Output: 6
+# Explanation: [4,-1,2,1] has the largest sum = 6.
+# Follow up:
+#
+# If you have figured out the O(n) solution, try coding another solution using the divide and conquer approach, which is more subtle.
+class Solution:
+    def maxSubArray(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        m = nums[0]
+        pre = m
+        for i in range(1, len(nums)):
+            pre = max(pre + nums[i], nums[i])
+            if pre > m:
+                m = pre
+        return m
